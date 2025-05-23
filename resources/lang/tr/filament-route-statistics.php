c79786db6b90a9006f6c835347c968374fd8ace5
Bora Alp Arat <boralp@users.noreply.github.com>
2025-05-23 15:33:45 +0300

    Create filament-route-statistics.php

---
+++ resources/lang/tr/filament-route-statistics.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+
+    'table.columns.id' => 'Kimlik',
+    'table.columns.user' => 'Kullanıcı',
+    'table.columns.user.name' => 'Kullanıcı',
+    'table.columns.team' => 'Takım',
+    'table.columns.method' => 'Yöntem',
+    'table.columns.route' => 'Rota',
+    'table.columns.status' => 'Durum',
+    'table.columns.ip' => 'IP',
+    'table.columns.date' => 'Tarih',
+    'table.columns.counter' => 'Sayaç',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Widgets
+    |--------------------------------------------------------------------------
+    */
+    'widgets.overview.statistic.description' => 'İstek sayısı',
+];
